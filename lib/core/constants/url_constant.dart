73d347a06256cffa86bc55a777b7dfc2a34d3529
Sajad <sajadrahimi0101@gmail.com>
2023-08-26 12:26:36 +0330

    transactions connected to backend

--- lib/core/constants/url_constant.dart
+++ lib/core/constants/url_constant.dart
@@ -2,3 +2,4 @@ const String baseUrl = 'https://27kingmovi.xyz/api/?type';
 const String homeUrl = '$baseUrl=home';
 const String loginUrl = '$baseUrl=login';
 const String ticketUrl = '$baseUrl=ticket';
+const String transactionUrl = '$baseUrl=trans';
